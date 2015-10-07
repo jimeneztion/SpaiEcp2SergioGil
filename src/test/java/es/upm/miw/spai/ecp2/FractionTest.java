08fd19fc48f993a026e8ed1afb0e7800762be278
Mona <monashh_m@hotmail.com>
2015-10-07 04:47:20 +0200

    Creado test testIsSmaller en la clase FractionTest

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class FractionTest {
 	private Fraction fr;
+	private Fraction fr2;
 
 	@Before
 	public void before() {
@@ -36,5 +37,11 @@ public class FractionTest {
 		fr = new Fraction(3, 0);
 		assertEquals(Double.POSITIVE_INFINITY, fr.decimal(), 10e-1);
 	}
+	
+	@Test
+    public void testIsSmaller() {
+        fr2 = new Fraction(2, 3);
+        assertEquals(true, fr.isSmaller(fr2));
+    }
 
 }
